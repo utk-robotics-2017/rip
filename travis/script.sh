938999e892d048aa26822e54c0d7e292d2fd455f
astewa44 <astewa44@utk.edu>
2017-10-24 22:41:37 -0400

    Setup testing for Pins. (Probably)

--- travis/script.sh
+++ travis/script.sh
@@ -1,38 +1,54 @@
 #!/usr/bin/env bash
+set -e
+
+cd build
+
 # Checks if it compiles
 make -j4
 
 if [[ $2 ]]; then
-	BRANCH = $1
+	BRANCH=$1
 else
-	BRANCH = $3
+	BRANCH=$3
 fi
 
 # Runs cpp check
 if [[ $BRANCH == "arduino_gen/"* ]]; then
+	cd arduino_gen
 	./arduino_gen_test
-	cd ../arduino_gen
-else if [[ $BRANCH == "pathfinder/"* ]]; then
+	cd ../../arduino_gen
+elif [[ $BRANCH == "pathfinder/"* ]]; then
+	cd pathfinder
 	./pathfinder_test
-	cd ../core/navigation/pathfinder
-else if [[ $BRANCH == "roboclaw/"* ]]; then
+	cd ../../core/navigation/pathfinder
+elif [[ $BRANCH == "roboclaw/"* ]]; then
+	cd roboclaw
 	./roboclaw_test
-	cd ../utilities/roboclaw
-else if [[ $BRANCH == "cmd_messenger/"* ]]; then
+	cd ../../utilities/roboclaw
+elif [[ $BRANCH == "cmd_messenger/"* ]]; then
+	cd cmd_messenger
 	./cmd_messenger_test
-	cd ../utilities/cmd_messenger
-else if [[ $BRANCH == "pathman/"* ]]; then
+	cd ../../utilities/cmd_messenger
+elif [[ $BRANCH == "pathman/"* ]]; then
+	cd pathman
 	./pathman_test
-	cd ../utilities/pathman
-else if [[ $BRANCH == "navx/"* ]]; then
+	cd ../../utilities/pathman
+elif [[ $BRANCH == "navx/"* ]]; then
+	cd navx
 	./navx_test
-	cd ../core/navigation/navx
-else if [[ $BRANCH == "communication/"* ]]; then
+	cd ../../core/navigation/navx
+elif [[ $BRANCH == "communication/"* ]]; then
+	cd communication
 	./communication_test
-	cd ../core/communication
-else if [[ $BRANCH == "appendages/"* ]]; then
+	cd ../../core/communication
+elif [[ $BRANCH == "appendages/"* ]]; then
+	cd appendages
 	./appendages_test
-	cd ../appendages
+	cd ../../appendages
+elif [[ $BRANCH == "pins/"* ]]; then
+	cd utilities/pins
+	./pins_test
+	cd ../../utilities/pins
 fi
 
 cppcheck src/*.cpp include/*.hpp
