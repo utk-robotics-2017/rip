e3f91616f00b52657c069ab4632f4388d4f777d3
thisguyistotallyben <thisguyistotallyben@gmail.com>
2017-11-14 19:23:28 -0500

    namespaces unified across pins cpp code

--- utilities/pins/src/gpio.cpp
+++ utilities/pins/src/gpio.cpp
@@ -21,67 +21,70 @@ std::ostream& operator<<(std::ostream& estream, const rip::pins::gpio::DigitalPi
 
 namespace rip
 {
-    namespace pins
+    namespace utilities
     {
-        namespace gpio
+        namespace pins
         {
-            void setPinMode(uint8_t pin, DigitalPinMode mode)
+            namespace gpio
             {
-                std::ofstream file ("/sys/class/gpio/export", std::ofstream::out);
-                if (file.fail() || !file.is_open())
+                void setPinMode(uint8_t pin, DigitalPinMode mode)
                 {
-                    throw FileAccessError("Cannot access /sys/class/gpio/export");
-                }
-                file << std::to_string(pin);
-                file.close();
+                    std::ofstream file ("/sys/class/gpio/export", std::ofstream::out);
+                    if (file.fail() || !file.is_open())
+                    {
+                        throw FileAccessError("Cannot access /sys/class/gpio/export");
+                    }
+                    file << std::to_string(pin);
+                    file.close();
 
-                file.open(fmt::format("/sys/class/gpio/gpio{}/direction", pin), std::ofstream::out);
-                if (file.fail() || !file.is_open())
-                {
-                    throw FileAccessError(fmt::format("Cannot access /sys/class/gpio{}/direction", pin));
-                }
+                    file.open(fmt::format("/sys/class/gpio/gpio{}/direction", pin), std::ofstream::out);
+                    if (file.fail() || !file.is_open())
+                    {
+                        throw FileAccessError(fmt::format("Cannot access /sys/class/gpio{}/direction", pin));
+                    }
 
-                switch (mode)
-                {
-                    case DigitalPinMode::kInput:
-                        file << "in";
-                        break;
-                    case DigitalPinMode::kOutput:
-                        file << "out";
-                        break;
-                }
-                file.close();
-            }
-
-            void digitalWrite(uint8_t pin, DigitalPinValue value)
-            {
-                std::ofstream file(fmt::format("/sys/class/gpio/gpio{}/value", pin));
-                if (file.fail() || !file.is_open())
-                {
-                    throw FileAccessError(fmt::format("Cannot access /sys/class/gpio/gpio{}/value", pin));
+                    switch (mode)
+                    {
+                        case DigitalPinMode::kInput:
+                            file << "in";
+                            break;
+                        case DigitalPinMode::kOutput:
+                            file << "out";
+                            break;
+                    }
+                    file.close();
                 }
 
-                file << value; // look at the overloaded operator for DigitalPinValue
-                file.close();
-            }
-
-            DigitalPinValue digitalRead(uint8_t pin)
-            {
-                std::ifstream file(fmt::format("/sys/class/gpio/gpio{}/value", pin));
-                if (file.fail() || !file.is_open())
+                void digitalWrite(uint8_t pin, DigitalPinValue value)
                 {
-                    throw FileAccessError(fmt::format("Cannot open /sys/class/gpio/gpio{}/value for reading", pin));
+                    std::ofstream file(fmt::format("/sys/class/gpio/gpio{}/value", pin));
+                    if (file.fail() || !file.is_open())
+                    {
+                        throw FileAccessError(fmt::format("Cannot access /sys/class/gpio/gpio{}/value", pin));
+                    }
+
+                    file << value; // look at the overloaded operator for DigitalPinValue
+                    file.close();
                 }
 
-                std::string value;
-                file >> value;
-                file.close();
-                if (value.size() != 1)
+                DigitalPinValue digitalRead(uint8_t pin)
                 {
-                    throw DigitalReadError("Bad size of read from /s/c/g/X/value descriptor.");
+                    std::ifstream file(fmt::format("/sys/class/gpio/gpio{}/value", pin));
+                    if (file.fail() || !file.is_open())
+                    {
+                        throw FileAccessError(fmt::format("Cannot open /sys/class/gpio/gpio{}/value for reading", pin));
+                    }
+
+                    std::string value;
+                    file >> value;
+                    file.close();
+                    if (value.size() != 1)
+                    {
+                        throw DigitalReadError("Bad size of read from /s/c/g/X/value descriptor.");
+                    }
+                    return static_cast<DigitalPinValue>(value[0] - '0');
                 }
-                return static_cast<DigitalPinValue>(value[0] - '0');
-            }
-        }
-    }
+            } // gpio
+        } // pins
+    } // utilities
 } // rip
--- utilities/pins/src/spi.cpp
+++ utilities/pins/src/spi.cpp
@@ -5,175 +5,181 @@
 
 namespace rip
 {
-    namespace pins
+    namespace utilities
     {
-        Spi::Spi()
-            : Spi(kDefaultDevice)
-        {}
-
-        Spi::Spi(const std::string& device)
-            : m_device(device)
+        namespace pins
         {
-            open();
-        }
-
-        void Spi::open()
-        {
-            if (m_fd)
+            namespace spi
             {
-                close();
-                m_fd = 0;
-            }
+                Spi::Spi()
+                    : Spi(kDefaultDevice)
+                {}
 
-            if ((m_fd = open(m_device.c_str(), O_RDWR)) < 0)
-            {
-                system("modprobe spi-bcm2835");
-                sleep(1);
-                if ((m_fd = open(m_device.c_str(), O_RDWR)) < 0)
+                Spi::Spi(const std::string& device)
+                    : m_device(device)
                 {
-                    throw OpenningError(fmt::format("Error opening {}\n", m_device));
+                    open();
                 }
-            }
-        }
 
-        Spi::~Spi()
-        {
-            close();
-        }
-
-        void Spi::close()
-        {
-            if (m_fd)
-            {
-                close(m_fd);
-            }
-        }
-
-        void Spi::setCS(int cs)
-        {
-            m_device = fmt::format("/dev/spidev0.{}", cs);
-            if ((m_fd = open(m_device.c_str(), O_RDWR)) < 0)
-            {
-                system(fmt::format("modprobe spi-bcm2835 cs_pin={}", cs));
-                sleep(1);
-                if ((m_fd = open(m_device.c_str(), O_RDWR)) < 0)
+                void Spi::open()
                 {
-                    throw
+                    if (m_fd)
+                    {
+                        close();
+                        m_fd = 0;
+                    }
+
+                    if ((m_fd = open(m_device.c_str(), O_RDWR)) < 0)
+                    {
+                        system("modprobe spi-bcm2835");
+                        sleep(1);
+                        if ((m_fd = open(m_device.c_str(), O_RDWR)) < 0)
+                        {
+                            throw OpenningError(fmt::format("Error opening {}\n", m_device));
+                        }
+                    }
                 }
-            }
-        }
 
-        void Spi::setMode(uint8_t mode)
-        {
-            if (ioctl(m_fd, SPI_IOC_WR_MODE, &mode) < 0)
-            {
-                throw SpiError(fmt::format("Error with setting mode {}", mode));
-            }
-        }
+                Spi::~Spi()
+                {
+                    close();
+                }
 
-        uint8_t Spi::getMode()
-        {
-            __u8 mode;
-            if (iotcl(m_fd, SPI_IOC_RD_MODE, &mode) < 0)
-            {
-                throw SpiError(fmt::format("Error with reading mode"));
-            }
-            return mode;
-        }
+                void Spi::close()
+                {
+                    if (m_fd)
+                    {
+                        close(m_fd);
+                    }
+                }
 
-        void Spi::setLSB(uint8_t lsb)
-        {
-            if (ioctl(m_fd, SPI_IOC_WR_LSB_FIRST, &lsb) < 0)
-            {
-                throw SpiError(fmt::format("Error with setting lsb {}", lsb));
-            }
-        }
+                void Spi::setCS(int cs)
+                {
+                    m_device = fmt::format("/dev/spidev0.{}", cs);
+                    if ((m_fd = open(m_device.c_str(), O_RDWR)) < 0)
+                    {
+                        system(fmt::format("modprobe spi-bcm2835 cs_pin={}", cs));
+                        sleep(1);
+                        if ((m_fd = open(m_device.c_str(), O_RDWR)) < 0)
+                        {
+                            throw
+                        }
+                    }
+                }
 
-        uint8_t Spi::getLSB()
-        {
-            __u8 lsb;
-            if (iotcl(m_fd, SPI_IOC_RD_LSB_FIRST, &lsb) < 0)
-            {
-                throw SpiError(fmt::format("Error with reading lsb"));
-            }
-            return lsb;
-        }
+                void Spi::setMode(uint8_t mode)
+                {
+                    if (ioctl(m_fd, SPI_IOC_WR_MODE, &mode) < 0)
+                    {
+                        throw SpiError(fmt::format("Error with setting mode {}", mode));
+                    }
+                }
 
-        void Spi::setLength(uint8_t bits)
-        {
-            if (ioctl(m_fd, SPI_IOC_WR_BITS_PER_WORD, &bits) < 0)
-            {
-                throw SpiError(fmt::format("Error with setting length {}", bits));
-            }
-        }
+                uint8_t Spi::getMode()
+                {
+                    __u8 mode;
+                    if (iotcl(m_fd, SPI_IOC_RD_MODE, &mode) < 0)
+                    {
+                        throw SpiError(fmt::format("Error with reading mode"));
+                    }
+                    return mode;
+                }
 
-        uint8_t Spi::getLength()
-        {
-            __u8 bits;
-            if (iotcl(m_fd, SPI_IOC_RD_BITS_PER_WORD, &bits) < 0)
-            {
-                throw SpiError(fmt::format("Error with reading length"));
-            }
-            return bits;
-        }
+                void Spi::setLSB(uint8_t lsb)
+                {
+                    if (ioctl(m_fd, SPI_IOC_WR_LSB_FIRST, &lsb) < 0)
+                    {
+                        throw SpiError(fmt::format("Error with setting lsb {}", lsb));
+                    }
+                }
 
-        void Spi::setSpeed(uint8_t speed)
-        {
-            if (ioctl(m_fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed) < 0)
-            {
-                throw SpiError(fmt::format("Error with setting speed {}", speed));
-            }
-        }
+                uint8_t Spi::getLSB()
+                {
+                    __u8 lsb;
+                    if (iotcl(m_fd, SPI_IOC_RD_LSB_FIRST, &lsb) < 0)
+                    {
+                        throw SpiError(fmt::format("Error with reading lsb"));
+                    }
+                    return lsb;
+                }
 
-        uint8_t Spi::getLength()
-        {
-            __u8 speed;
-            if (iotcl(m_fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed) < 0)
-            {
-                throw SpiError(fmt::format("Error with reading speed"));
-            }
-            return speed;
-        }
+                void Spi::setLength(uint8_t bits)
+                {
+                    if (ioctl(m_fd, SPI_IOC_WR_BITS_PER_WORD, &bits) < 0)
+                    {
+                        throw SpiError(fmt::format("Error with setting length {}", bits));
+                    }
+                }
 
-        std::vector<uint8_t> Spi::read(size_t length)
-        {
-            if (length < 1)
-            {
-                throw ReadError("Length must be 1 or greater");
-            }
+                uint8_t Spi::getLength()
+                {
+                    __u8 bits;
+                    if (iotcl(m_fd, SPI_IOC_RD_BITS_PER_WORD, &bits) < 0)
+                    {
+                        throw SpiError(fmt::format("Error with reading length"));
+                    }
+                    return bits;
+                }
 
-            if (!m_fd)
-            {
-                open()
-            }
+                void Spi::setSpeed(uint8_t speed)
+                {
+                    if (ioctl(m_fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed) < 0)
+                    {
+                        throw SpiError(fmt::format("Error with setting speed {}", speed));
+                    }
+                }
 
-            uint8_t* buffer = new uint8_t[length];
-            if (read(m_fd, buffer, length) != length)
-            {
-                throw ReadError(fmt::format("SPI read error. Device: {}\n", m_device));
-            }
+                uint8_t Spi::getLength()
+                {
+                    __u8 speed;
+                    if (iotcl(m_fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed) < 0)
+                    {
+                        throw SpiError(fmt::format("Error with reading speed"));
+                    }
+                    return speed;
+                }
 
-            return std::vector<uint8_t>(buffer, buffer + length);
-        }
+                std::vector<uint8_t> Spi::read(size_t length)
+                {
+                    if (length < 1)
+                    {
+                        throw ReadError("Length must be 1 or greater");
+                    }
 
-        void Spi::write(std::vector<uint8_t> message)
-        {
-            size_t length = message.size();
-            if (length < 1)
-            {
-                return;
-            }
+                    if (!m_fd)
+                    {
+                        open()
+                    }
 
-            spi_ioc_transfer xfer;
-            xfer.tx_buf = &message[0];
-            xfer.len = length;
-            xfer.rx_bug = 0;
-            xfer.delay_usecs = 0;
-            xfer.speed_hz = 0;
-            xfer.bits_per_word = 0;
-            if (ioctl(m_fd, SPI_IOC_MESSAGE(1), &xfer) < 0)
-            {
-                throw WriteError();
+                    uint8_t* buffer = new uint8_t[length];
+                    if (read(m_fd, buffer, length) != length)
+                    {
+                        throw ReadError(fmt::format("SPI read error. Device: {}\n", m_device));
+                    }
+
+                    return std::vector<uint8_t>(buffer, buffer + length);
+                }
+
+                void Spi::write(std::vector<uint8_t> message)
+                {
+                    size_t length = message.size();
+                    if (length < 1)
+                    {
+                        return;
+                    }
+
+                    spi_ioc_transfer xfer;
+                    xfer.tx_buf = &message[0];
+                    xfer.len = length;
+                    xfer.rx_bug = 0;
+                    xfer.delay_usecs = 0;
+                    xfer.speed_hz = 0;
+                    xfer.bits_per_word = 0;
+                    if (ioctl(m_fd, SPI_IOC_MESSAGE(1), &xfer) < 0)
+                    {
+                        throw WriteError();
+                    }
+                }
             }
         }
     }
