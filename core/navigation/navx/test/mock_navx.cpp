16d95ac6de7b361bea814b3d18eacb9800b2aca4
solsane <solsane@gmail.com>
2018-01-30 17:31:04 -0500

    Prep for testing branch

---
+++ core/navigation/navx/test/mock_navx.cpp
@@ -0,0 +1,21 @@
+
+#include "mock_navx.hpp"
+#include <AHRS.h>
+
+namespace rip
+{
+    namespace navigation
+    {
+        namespace navx
+        {
+            namespace mocks
+            {
+                class MockRoboclaw
+                {
+                public:
+                    
+                };
+            }
+        }
+    }
+}
---
+++ core/navigation/navx/test/test_navx.cpp
@@ -0,0 +1,21 @@
+#include "mock_navx.hpp"
+#include <exceptions.hpp>
+#include <memory>
+#include <gtest/gtest.h>
+
+namespace rip
+{
+    namespace navigation
+    {
+        namespace navx
+        {
+            namespace tests
+            {
+                TEST(categoryname, specifictestname)
+                {
+                    // do me
+                }
+            }
+        }
+    }
+}
