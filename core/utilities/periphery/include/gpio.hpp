a5114072d192f604aa5f294c508f548002d5559f
Ian Lumsden <lumsden.ian@gmail.com>
2018-02-08 22:09:48 -0500

    Adds the initial completed version of gpio.hpp.

---
+++ core/utilities/periphery/include/gpio.hpp
@@ -0,0 +1,52 @@
+#ifndef _PERIPHERY_GPIO_HPP
+#define _PERIPHERY_GPIO_HPP
+
+extern "C"
+{
+    #include "gpio.h"
+}
+
+namespace rip
+{
+
+    namespace periphery
+    {
+
+        class gpio
+        {
+            public:
+
+                void open(unsigned int pin, int direction);
+
+                bool read();
+
+                void write(bool value);
+
+                void poll(int timeout_ms);
+
+                void close();
+
+                bool supports_interrupts();
+
+                int get_direction();
+
+                int get_edge();
+
+                void set_direction(int direction);
+
+                void set_edge(int edge);
+
+                unsigned int pin();
+
+                int fd();
+
+                std::string tostring(size_t len);
+
+            private:
+
+                gpio_handle gpio;
+        };
+
+    }
+
+}
