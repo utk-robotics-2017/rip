e69cc92b1a78bd9f0c2a39ff966428eae32f9818
Ian Lumsden <lumsden.ian@gmail.com>
2018-02-12 13:44:03 -0500

    Fixes some formatting.

--- core/utilities/periphery/include/spi.hpp
+++ core/utilities/periphery/include/spi.hpp
@@ -16,23 +16,40 @@ namespace rip
         {
             public:
                 void open(const std::string path, unsigned int mode, uint32_t max_speed);
+
                 void openAdvanced(const std::string path, unsigned int mode, uint32_t max_speed,
                                    int bit_order, uint8_t bits_per_word, uint8_t extra_flags);
+
                 void transfer(const uint8_t *txbuf, uint8_t *rxbuf, size_t len);
+
                 void close();
+
                 unsigned int getMode();
+
                 uint32_t getMaxSpeed();
+
                 int getBitOrder();
+
                 uint8_t getBitsPerWord();
+
                 uint8_t getExtraFlags();
+
                 void setMode(unsigned int mode);
+
                 void setMaxSpeed(uint32_t max_speed);
+
                 void setBitOrder(int bit_order);
+
                 void setBitsPerWord(uint8_t bits_per_word);
+
                 void setExtraFlags(uint8_t extra_flags);
+
                 int fd();
+
                 std::string toString(size_t len);
+
             private:
+
                 spi_t spi;
         };
 
