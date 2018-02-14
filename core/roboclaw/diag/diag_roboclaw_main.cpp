a8c84c9683ff58af1a450162f2c4cad3936aa7aa
Andrew Messing <amessing@utk.edu>
2018-02-13 20:26:21 -0500

    fixed some issues with roboclaw diag

--- core/roboclaw/diag/diag_roboclaw_main.cpp
+++ core/roboclaw/diag/diag_roboclaw_main.cpp
@@ -2,29 +2,30 @@
 #include <iostream>
 #include <inttypes.h>
 #include <serial.h>
-#include <units.hpp>
+#include <units/units.hpp>
 #include "diag_roboclaw.hpp"
 
-using namespace rip::utilities::roboclaw;
+using namespace rip::roboclaw;
 /*
 Interactive diagnostic tests, specifically for the Roboclaw's on the demo bot (for now)
 DO NOT run without physical access, and powered on via battery.
 Killing this program will not save you from the malice of motors.
 */
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     //make serial stuff
-    nlohmann::json config1={
+    nlohmann::json config1 =
+    {
         {"address", 0x80},
         {"timeout", 1000},
         {"ticks_per_rev", 3591.84},
-        {"wheel_radius", (2.875/2.0) * rip::units::in},
+        {"wheel_radius", (2.875 / 2.0)* rip::units::in},
         {"device", "/dev/ttyAMA0"},
         {"baudrate", 115200}
     };
     nlohmann::json config2 = config1;
-    config2["address"]=0x81;
+    config2["address"] = 0x81;
 
     Roboclaw testClawLeft(config2);
     Roboclaw testClawRight(config1);
