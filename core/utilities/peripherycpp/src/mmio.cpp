ddfb40d88f1e71f20e6b2e7730f847edcec00c0b
Ian Lumsden <lumsden.ian@gmail.com>
2018-02-13 20:14:42 -0500

    Adds the first full versions for mmio (no error checking).

--- core/utilities/peripherycpp/src/mmio.cpp
+++ core/utilities/peripherycpp/src/mmio.cpp
@@ -43,58 +43,64 @@ namespace rip
 
         std::vector<uint8_t> Mmio::read(uintptr_t offset, size_t len)
         {
-            uint8_t *buf;
-            int err_num = mmio_read(&mmio, offset, buf, len);
+            std::vector<uint8_t> buf(static_cast<int>(len));
+            int err_num = mmio_read(&mmio, offset, &buf[0], len);
             // Add error checking
-            int i = 0;
-            std::vector<uint8_t> value;
-            while (i < len)
-            {
-                value.push_back(*buf);
-                ++buf;
-                i++;
-            }
-            return value;
+            return buf;
         }
 
         void Mmio::write32(uintptr_t offset, uint32_t value)
         {
-
+            int err_num = mmio_write32(&mmio, offset, value);
+            // Add error checking
+            return;
         }
 
         void Mmio::write16(uintptr_t offset, uint16_t value)
         {
-
+            int err_num = mmio_write16(&mmio, offset, value);
+            // Add error checking
+            return;
         }
 
         void Mmio::write8(uintptr_t offset, uint8_t value)
         {
-
+            int err_num = mmio_write8(&mmio, offset, value);
+            // Add error checking
+            return;
         }
 
         void Mmio::write(uintptr_t offset, std::vector<uint8_t> &buf)
         {
-
+            int err_num = mmio_write(&mmio, offset, &buf[0], buf.size());
+            // Add error checking
+            return;
         }
 
         void Mmio::close()
         {
-
+            int err_num = mmio_close(&mmio);
+            // Add error checking
+            return;
         }
 
         uintptr_t Mmio::base()
         {
-
+            return mmio_base(&mmio);
         }
 
         size_t size()
         {
-
+            return mmio_size(&mmio);
         }
 
-        std::string toString()
+        std::string toString(size_t len)
         {
-
+            char *cstr;
+            int err_num = mmio_tostring(&mmio, cstr, len);
+            // Add error checking
+            string str = cstr;
+            return str;
         }
 
     }
