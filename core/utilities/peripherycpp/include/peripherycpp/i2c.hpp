81f62ca96cf0cd89e9e8e5f28b9948307dcf3c60
Ian Lumsden <lumsden.ian@gmail.com>
2018-02-18 17:13:40 -0500

    Adds Doxygen comments to I2C.

--- core/utilities/peripherycpp/include/peripherycpp/i2c.hpp
+++ core/utilities/peripherycpp/include/peripherycpp/i2c.hpp
@@ -19,14 +19,39 @@ namespace rip
         {
             public:
 
+                /**
+                 * open
+                 * @param path  the path to the desired i2c-dev device. 
+                 * @brief  Open the i2c-dev device at the specified path.
+                 */
                 void open(const std::string path);
 
+                /**
+                 * transfer
+                 * @param msg_data  the data for the messages that will be transferred (must be of size count). Each element of the vector will be used for a different message."
+                 * @param flags  the flags for the messages that will be trnasferred (must be of size count). Each element of the vector must correspond with the same indexed element of msg_data.
+                 * @param count  the number of messages to be transferred.
+                 * @brief  Transfer count number of struct i2c_msg I2C messages.
+                 */
                 void transfer(std::vector< std::vector<uint8_t> > msg_data, std::vector<int> flags, size_t count);
 
+                /**
+                 * close
+                 * @brief  Close the i2c-dev device.
+                 */
                 void close();
 
+                /**
+                 * fd
+                 * @brief  Return the file descriptor (for the underlying i2c-dev device) of the I2C handle.
+                 */
                 int fd();
 
+                /**
+                 * toString
+                 * @param len  the size of the string to be returned.
+                 * @brief  Return a string representation of the I2C handle.
+                 */
                 std::string toString(size_t len);
 
             private:
