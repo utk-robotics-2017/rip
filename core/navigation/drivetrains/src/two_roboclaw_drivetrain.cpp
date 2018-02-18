f0d9a8ad1cc0f5bdfc9c42c6955e2d858695bb7a
solsane <solsane@gmail.com>
2018-02-18 17:23:16 -0500

    copypasta

---
+++ core/navigation/drivetrains/src/two_roboclaw_drivetrain.cpp
@@ -0,0 +1,42 @@
+#include "two_roboclaw_drivetrain.hpp"
+
+namespace rip
+{
+    namespace subsystem
+    {
+        void TwoRoboclawDriveTrain::drive(double power)
+        {
+
+        }
+
+        void TwoRoboclawDriveTrain::drive(double left, double right)
+        {
+
+        }
+
+        void TwoRoboclawDriveTrain::drive(double front_left, double front_right, double back_left, double back_rightk)
+        {
+
+        }
+
+        void TwoRoboclawDriveTrain::drive(const NavCommand& command)
+        {
+
+        }
+
+        void TwoRoboclawDriveTrain::drive(const NavCommand& left, const NavCommand& right)
+        {
+
+        }
+
+        void TwoRoboclawDriveTrain::drive(const NavCommand& front_left, const NavCommand& front_right, const NavCommand& back_left, const NavCommand& back_right)
+        {
+
+        }
+
+        void TwoRoboclawDriveTrain::stop(bool brake = false)
+        {
+
+        }
+    }//subsystem
+}//rip
