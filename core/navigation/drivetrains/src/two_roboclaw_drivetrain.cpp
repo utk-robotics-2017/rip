ba60afe88068364d1d323b244547f38d8a9416a3
Aiden <solsane@gmail.com>
2018-02-18 17:46:13 -0500

    implemented basic 2rclaw drivetrain methods

--- core/navigation/drivetrains/src/two_roboclaw_drivetrain.cpp
+++ core/navigation/drivetrains/src/two_roboclaw_drivetrain.cpp
@@ -6,17 +6,23 @@ namespace rip
     {
         void TwoRoboclawDriveTrain::drive(double power)
         {
-
+            m_left->drive(32767 * power);
+            m_right->drive(32767 * power)
         }
 
         void TwoRoboclawDriveTrain::drive(double left, double right)
         {
-
+            m_left->drive(32767 * left);
+            m_right->drive(32767 * right);
         }
 
         void TwoRoboclawDriveTrain::drive(double front_left, double front_right, double back_left, double back_rightk)
         {
+            m_left->drive(Roboclaw::Motor::kM1, 32767 * power);
+            m_right->drive(Roboclaw::Motor::kM1, 32767 * power);
 
+            m_left->drive(Roboclaw::Motor::kM2, 32767 * power);
+            m_right->drive(Roboclaw::Motor::kM2, 32767 * power);
         }
 
         void TwoRoboclawDriveTrain::drive(const NavCommand& command)
