7f84086356e9b2a3bb021f24ea9c3950e546369d
Ian Lumsden <lumsden.ian@gmail.com>
2018-02-18 17:05:09 -0500

    Adds Doxygen comments to GPIO.

--- core/utilities/peripherycpp/include/peripherycpp/gpio.hpp
+++ core/utilities/peripherycpp/include/peripherycpp/gpio.hpp
@@ -17,30 +17,89 @@ namespace rip
         {
             public:
 
+                /**
+                 * open
+                 * @param pin  the Linux GPIO pin number for the pin being opened.
+                 * @param direction  an integer corresponding to the direction the GPIO pin is being opened (it will be converted to an enum).
+                 * @brief  Open the sysfs GPIO corresponding to the specified pin, with the specified direction.
+                 */
                 void open(unsigned int pin, int direction);
 
+                /**
+                 * read
+                 * @brief  Read the state of the GPIO and return it.
+                 */
                 bool read();
 
+                /**
+                 * write
+                 * @param value  the value that will be used to set the state of the GPIO.
+                 * @brief  Set the state of the GPIO to value.
+                 */
                 void write(bool value);
 
+                /**
+                 * poll
+                 * @param timeout_ms  what it does depends on the value. If it is positive, it is a timeout in milliseconds. If 0, it is a non-blocking poll. If negative, it is a blocking poll.
+                 * @brief  Poll a GPIO for the edge event configured with gpio_set_edge.
+                 */
                 int poll(int timeout_ms);
 
+                /**
+                 * close
+                 * @brief  Close the sysfs GPIO.
+                 */
                 void close();
 
+                /**
+                 * supportsInterrupts
+                 * @brief Query a GPIO for edge interrupt support.
+                 */
                 bool supportsInterrupts();
 
+                /**
+                 * getDirection
+                 * @brief  Query the configured direction of the GPIO. The direction is returned as an int of range [0, 4].
+                 */
                 int getDirection();
 
+                /**
+                 * getEdge
+                 * @brief  Query the configured edge of the GPIO. The edge is returned as an int of range [0, 3].
+                 */
                 int getEdge();
 
+                /**
+                 * setDirection
+                 * @param direction  the direction to be used in setting the GPIO, coded as an int of range [0. 4].
+                 * @brief  Set the direction of the GPIO.
+                 */
                 void setDirection(int direction);
 
+                /**
+                 * setEdge
+                 * @param edge  the edge to be used in setting the GPIO, coded as an int of range [0, 3].
+                 * @brief  Set the interrupt edge of the GPIO.
+                 */
                 void setEdge(int edge);
 
+                /**
+                 * pin
+                 * @brief  Return the pin the GPIO handle was opened with.
+                 */
                 unsigned int pin();
 
+                /**
+                 * fd
+                 * @brief  Return the file descriptor (for the underlying sysfs GPIO "value" file) of the GPIO handle.
+                 */
                 int fd();
 
+                /**
+                 * toString
+                 * @param len  the length of the returned string.
+                 * @brief  Return a string representation of the GPIO handle.
+                 */
                 std::string toString(size_t len);
 
             private:
