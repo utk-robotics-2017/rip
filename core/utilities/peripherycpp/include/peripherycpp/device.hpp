cd1ade141ff10d29ad7a2bf10a7cb6d7cc0f327e
Ian <lumsden.ian@gmail.com>
2018-02-20 16:43:55 -0500

    Adds a skeleton file for creating a parent class for the periphery devices.

---
+++ core/utilities/peripherycpp/include/peripherycpp/device.hpp
@@ -0,0 +1,39 @@
+#ifndef _PERIPHERY_DEVICE_HPP
+#define _PERIPHERY_DEVICE_HPP
+
+#include <vector>
+#include <string>
+
+namespace rip
+{
+
+    namespace peripherycpp
+    {
+        
+        class Device
+        {
+            public:
+
+                //void open() = 0;
+
+                //openAdvanced() = 0;
+
+                //read() = 0;
+
+                //transfer() = 0;
+
+                //write() = 0;
+
+                //poll() = 0;
+
+                //void close() = 0;
+
+                //fd() = 0;
+
+                //toString = 0;
+
+        };
+
+    }
+
+}
