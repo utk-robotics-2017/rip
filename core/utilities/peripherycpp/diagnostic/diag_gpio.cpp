5fd4dd33ff9362ecb5fe0c9633d54315f42bdf6a
Ian Lumsden <lumsden.ian@gmail.com>
2018-02-20 18:59:35 -0500

    Adds a diagnostic test for gpio.

--- core/utilities/peripherycpp/diagnostic/diag_gpio.cpp
+++ core/utilities/peripherycpp/diagnostic/diag_gpio.cpp
@@ -11,7 +11,17 @@ int main(int argc, char **argv)
     g.close();
     g.open(2, 1);
     bool tmp = g.read();
+    unsigned int pnum = g.pin();
+    int psuc = g.poll(50);
+    bool support = g.supportsInterrupts();
+    int dir = g.getDirection();
+    int edge = g.getEdge();
     g.close();
     fprintf(stdout, "read produced %b\n", tmp);
-    
+    fprintf(stdout, "pin number = %i\n", pnum);
+    fprintf(stdout, "Poll Successful?: %i\n", psuc);
+    fprintf(stdout, "Supports Interrupts?: %i\n", support);
+    fprintf(stdout, "Direction code: %i\n", dir);
+    fprintf(stdout, "Edge code: %i\n", edge);
+    return 0;
 }
