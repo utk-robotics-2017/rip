5c60a10e717fdc4c21efd27a5cd40a9db0a588ff
solsane <solsane@gmail.com>
2018-02-22 12:31:43 -0500

    turn to angle starts, navx and relative version

--- core/navigation/actions/include/navigation_actions/turn_to_angle.hpp
+++ core/navigation/actions/include/navigation_actions/turn_to_angle.hpp
@@ -2,8 +2,11 @@
 #define TURN_TO_ANGLE_HPP
 
 #include <json.hpp>
-
+#include <drivetrains/drivetrain.hpp>
+#include <chrono>
 #include <framework/action.hpp>
+#include <navx/navx.hpp>
+#include <cmath>
 
 namespace rip
 {
@@ -11,43 +14,49 @@ namespace rip
     {
         namespace actions
         {
+            using NavX = navx::NavX;
             class TurnToAngle : public framework::Action
             {
             public:
-
+                /**
+                 * turns to angle, relative to current angle, by driving
+                 * wheels in opposite directions at the same speed.
+                 * @param name       [description]
+                 * @param drivetrain [description]
+                 * @param speed      [description]
+                 * @param angle      [description]
+                 * @param navx       [description]
+                 * @param radius     [description]
+                 */
+                TurnToAngle(const std::string& name,
+                    std::shared_ptr<drivetrains::Drivetrain> drivetrain,
+                    const units::AngularVelocity& speed, const units::Angle& angle,
+                    std::shared_ptr<NavX> navx, units::Distance radius);
                 /**
                 * Returns whether or not the action has finished execution.
+                * Returns true when navX reports change in angle that was
+                * requested
                 */
-                virtual bool isFinished() override
-                {
-                    // todo
-                }
-
+                virtual bool isFinished() override;
                 /**
                  * Iteratively called until {@see Action#isFinished()} returns true
                  */
-                virtual void update(nlohmann::json& state) override
-                {
-                    // todo
-                }
-
+                virtual void update(nlohmann::json& state) override;
                 /**
                  * Run once before the main code
                  */
-                virtual void setup(nlohmann::json& state) override
-                {
-                    // todo
-                }
-
+                virtual void setup(nlohmann::json& state) override;
                 /**
                  * Run once after finished
                  */
-                virtual void teardown(nlohmann::json& state) override
-                {
-                    // todo
-                }
+                virtual void teardown(nlohmann::json& state) override;
 
             private:
+                units::AngularVelocity m_speed;
+                std::shared_ptr<drivetrains::Drivetrain> m_drivetrain;
+                std::shared_ptr<NavX> m_navx;
+                units::Angle m_desiredAngle;
+                units::Distance m_c2wRadius;
             };
         }
     }
--- core/navigation/actions/src/turn_to_angle.cpp
+++ core/navigation/actions/src/turn_to_angle.cpp
@@ -1,4 +1,6 @@
 #include "navigation_actions/turn_to_angle.hpp"
+#include <misc/logger.hpp>
+#include <fmt/format.h>
 
 namespace rip
 {
@@ -6,24 +8,61 @@ namespace rip
     {
         namespace actions
         {
-            TurnToAngle::isFinished()
-            {
+            TurnToAngle::TurnToAngle(const std::string& name,
+                std::shared_ptr<drivetrains::Drivetrain> drivetrain,
+                const units::AngularVelocity& speed, const units::Angle& angle,
+                std::shared_ptr<NavX> navx, units::Distance radius)
+                 : Action(name)
+                 , m_drivetrain(drivetrain)
+                 , m_speed(speed)
+                 , m_desiredAngle(angle)
+                 , m_navx(navx)
+                 , m_c2wRadius(radius)
+            {}
 
+            bool TurnToAngle::isFinished()
+            {
+                units::Angle currentAngle = m_navx->getAngle();
+
+                if(m_desiredAngle >= 0)
+                {
+                    return currentAngle >= m_desiredAngle;
+                }
+                else
+                {
+                    return currentAngle <= m_desiredAngle;
+                }
             }
 
-            TurnToAngle::update()
+            void TurnToAngle::update(nlohmann::json& state)
             {
-
+                return;
             }
 
-            TurnToAngle::setup()
+            void TurnToAngle::setup(nlohmann::json& state)
             {
-
+                //initial angle
+                misc::Logger::getInstance()->debug("setting yaw offset");
+                m_navx->zeroYaw();
+                motorcontrollers::MotorDynamics dynamicsLeft, dynamicsRight;
+                misc::Logger::getInstance()->debug(fmt::format("in place turn intended angular velocity (rev/min): {}"
+                , m_speed.to(units::rev / units::minute)));
+                if(m_speed() < 0)
+                {
+                    m_speed *= -1;
+                }
+                if(m_desiredAngle < 0)
+                {
+                    m_speed *= -1;
+                }
+                dynamicsLeft.setSpeed(m_speed * m_c2wRadius / units::rad);
+                dynamicsRight.setSpeed(-1 * m_speed * m_c2wRadius / units::rad);
+                m_drivetrain->drive(dynamicsLeft, dynamicsRight);
             }
 
-            TurnToAngle::teardown()
+            void TurnToAngle::teardown(nlohmann::json& state)
             {
-
+                m_drivetrain->stop();
             }
         }
     }
