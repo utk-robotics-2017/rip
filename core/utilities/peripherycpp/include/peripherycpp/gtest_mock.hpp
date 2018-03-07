308380d94c94e9f0613af3bff924fa0f25f47146
Cameron <cadkin17@vols.utk.edu>
2018-03-06 19:27:03 -0500

    Added serial mock to gmock header.

--- core/utilities/peripherycpp/include/peripherycpp/gtest_mock.hpp
+++ core/utilities/peripherycpp/include/peripherycpp/gtest_mock.hpp
@@ -55,7 +55,59 @@ namespace rip
                 MOCK_METHOD1(toString, std::string(size_t len));
         };
 
-        
+        class MockAbsSerial : public AbsSerial {
+            public:
+                MOCK_METHOD2(open, void(std::string device, unsigned int baudrate));
+
+                MOCK_METHOD2(read, std::vector<uint8_t>(size_t len, int timeout_ms));
+
+                MOCK_METHOD3(read, void(uint8_t* buf, size_t size, int timeout_ms));
+
+                MOCK_METHOD3(read, void(uint8_t* buf, size_t size, int timeout_ms));
+
+                MOCK_METHOD3(read, void(char* buf, size_t size, int timeout_ms));
+
+                MOCK_METHOD2(write, void(uint8_t* data, size_t size));
+
+                MOCK_METHOD2(write, void(char* data, size_t size));
+
+                MOCK_METHOD1(write, void(std::vector<uint8_t> data));
+                
+                MOCK_METHOD0(flush, void());
+
+                MOCK_METHOD0(outputWaiting, unsigned int());
+
+                MOCK_METHOD0(inputWaiting, unsigned int());
+                
+                MOCK_METHOD1(poll, bool(int timeout_ms));
+
+                MOCK_METHOD0(close, void());
+
+                MOCK_METHOD0(getBaudrate, uint32_t());
+
+                MOCK_METHOD0(getDatabits, unsigned int());
+
+                MOCK_METHOD0(getParity, int());
+
+                MOCK_METHOD0(getStopbits, unsigned int());
+
+                MOCK_METHOD0(getxOnxOff, bool());
+
+                MOCK_METHOD0(getRtscts, bool());
+
+                MOCK_METHOD1(setBaudrate, void(uint32_t baudrate));
+                
+                MOCK_METHOD1(setDatabits, void(unsigned int databits));
+
+                MOCK_METHOD1(setParity, void(int parity));
+
+                MOCK_METHOD1(setStopBits, void(unsigned int stopbits));
+
+                MOCK_METHOD1(setxOnxOff, void(bool enabled));
+
+                MOCK_METHOD1(setRtscts, void(bool enabled));
+        };
+
     }
 
 }
