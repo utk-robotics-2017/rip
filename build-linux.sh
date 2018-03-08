2f8ed9ff65f33e39d6b8eff9433eaa63db6fbd73
Ben Klein <robobenklein@gmail.com>
2018-03-08 11:39:43 -0500

    Build linux detect not enough ram on pis

--- build-linux.sh
+++ build-linux.sh
@@ -1,5 +1,23 @@
-#!/bin/bash
+#!/bin/zsh
+set -E
 pushd "$(dirname "$0")"
+
+case "$1" in
+  "--clean")
+    if [ -d build ]; then
+      echo "Removing $(dirname "$0")/build/"
+      rm -r $(dirname "$0")/build/
+    else
+      echo "No $(dirname "$0")/build/ to remove."
+    fi
+    shift
+    ;;
+  "--help")
+    echo "$0 [--clean|--help] <extra args are passed to cmake>"
+    popd
+    exit 0
+    ;;
+esac
 
 mkdir -p build
 pushd build
@@ -17,17 +35,27 @@ cmake .. \
   $@
 #
 
-nprocs="$(nproc --ignore=1)"
+# test if system has more than 1GB of RAM
+if (( $(cat /proc/meminfo | grep MemTotal | cut -d ' ' -f2- | tr -d ' [A-Za-z]' ) /1000 /1000 )); then
+  nprocs="$(nproc --ignore=1)"
+else
+  # system does not have enough RAM for more than 1 thread :<
+  nprocs="1"
+fi
+echo "Building with $nprocs threads."
 
 trap '' ERR
 make_retval=1
-make -j$(nproc --ignore=1) -l$(nproc --ignore=2)
+make -j$nprocs
 make_retval=$?
 trap 'SIGNAL=$?;cleanup' ERR
 
 if [ $make_retval -eq 0 ]; then
   echo "Make completed."
-else
+elif [ $nprocs -gt 1 ]; then
   echo -e "\033[0;31m !!! Multithreaded make has failed, building single-threaded for error isolation. \033[0m"
   make
+else
+  echo -e "\033[0;31m !!! Compilation failed. \033[0m"
 fi
+
