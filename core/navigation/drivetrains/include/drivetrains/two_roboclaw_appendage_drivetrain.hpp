4d51dde166778cf114ca5c64f08addf96ceaf514
Aiden <solsane@gmail.com>
2018-04-03 19:16:51 -0400

    Two roboclaw appendage compiles

---
+++ core/navigation/drivetrains/include/drivetrains/two_roboclaw_appendage_drivetrain.hpp
@@ -0,0 +1,102 @@
+#ifndef TWO_ROBOCLAW_DRIVE_TRAIN_HPP
+#define TWO_ROBOCLAW_DRIVE_TRAIN_HPP
+
+#include <memory>
+#include <appendages/roboclaw.hpp>
+#include <navx/navx.hpp>
+#include <units/units.hpp>
+#include "drivetrain.hpp"
+#include <motor_controllers/motor_dynamics.hpp>
+
+namespace rip
+{
+    namespace navigation
+    {
+        namespace drivetrains
+        {
+            /**
+             * Abstract base class for the drive train
+             */
+
+            class TwoRoboclawAppendageDrivetrain : public Drivetrain
+            {
+                using Roboclaw = rip::appendages::Roboclaw;
+                using NavX = navx::NavX;
+                using MotorDynamics = motorcontrollers::MotorDynamics;
+            public:
+                TwoRoboclawAppendageDrivetrain(const std::string& name, std::shared_ptr<Roboclaw> left,
+                     std::shared_ptr<Roboclaw> right, std::shared_ptr<NavX> navx = nullptr);
+
+                ~TwoRoboclawAppendageDrivetrain();
+
+                /**
+                 * Drive all the motors
+                 * @param power [-1.0, 1.0]
+                 */
+                virtual void drive(double power) override;
+
+                /**
+                 * Drive left and right separately
+                 * @param left [-1.0, 1.0]
+                 * @param right [-1.0, 1.0]
+                 */
+                virtual void drive(double left, double right) override;
+
+                /**
+                 * Drive all the motors
+                 *
+                 * all range from [-1.0, 1.0]
+                 */
+                virtual void drive(double front_left, double front_right, double back_left,
+                     double back_rightk) override;
+
+                /**
+                 * Single command to all motors
+                 */
+                virtual void drive(const MotorDynamics& command) override;
+
+                /**
+                 * Command left and right sides separately
+                 */
+                virtual void drive(const MotorDynamics& left, const MotorDynamics& right) override;
+
+                /**
+                 * Command four wheels separately
+                 */
+                virtual void drive(const MotorDynamics& front_left, const MotorDynamics& front_right,
+                const MotorDynamics& back_left, const MotorDynamics& back_right) override;
+
+                /**
+                * Reads encoders for every motor you tell it to read, reports back in respective
+                * order
+                */
+                virtual std::vector<units::Distance> readEncoders(const std::vector<Motor>& motors) override;
+                /**
+                * reads the encoder for one motor
+                */
+                virtual units::Distance readEncoder(const Motor& motor) override;
+                /**
+                * Reads encoder velocity for every motor you tell it to read, reports back in respective
+                * order
+                * @param motors list of motors to read
+                */
+                virtual std::vector<units::Velocity> readEncoderVelocities(const std::vector<Motor>& motors) override;
+                /**
+                * reads the encoder for one motor
+                */
+                virtual units::Velocity readEncoderVelocity(const Motor& motor) override;
+
+                virtual void stop() override;
+
+                virtual bool diagnostic() override;
+            private:
+
+                std::shared_ptr<Roboclaw> m_left;
+                std::shared_ptr<Roboclaw> m_right;
+                std::shared_ptr<NavX> m_navx;
+            };
+        }
+    }
+}
+
+#endif // DRIVE_TRAIN_HPP
