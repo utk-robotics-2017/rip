ded71f53febecc595f8d0b88ceca4e271dd12a23
Ian Lumsden <lumsden.ian@gmail.com>
2018-04-06 18:49:54 -0400

    Adds initial code for a Digital Output appendage.

---
+++ appendages/include/appendages/digital_output.hpp
@@ -0,0 +1,37 @@
+#ifndef DIGITAL_OUTPUT_HPP
+#define DIGITAL_OUTPUT_HPP
+
+#include <memory>
+#include <map>
+#include <string>
+
+#include <cmd_messenger/command.hpp>
+
+#include "appendages/appendage.hpp"
+#include "appendages/appendage_factory.hpp"
+
+#include <json.hpp>
+
+namespace rip
+{
+    namespace appendages
+    {
+        class DigitalOutput : public Appendage
+        {
+            public:
+            
+                void write(bool output);
+
+                virtual void stop() override;
+
+                virtual bool diagnostic() override;
+
+            protected:
+                friend class AppendageFactory;
+
+                static std::shared_ptr<Appendage> create(const nlohmann::json& config, const std::map<std::string, int>& command_map, std::shared_ptr<cmdmessenger::Device> device);
+
+                std::shared_ptr<cmdmessenger::Command> m_write;
+        };
+    }
+}
---
+++ appendages/src/digital_output.cpp
@@ -0,0 +1,42 @@
+#include "appendages/digital_output.hpp"
+
+#include <utility>
+#include <tuple>
+#include <memory>
+
+#include <cmd_messenger/cmd_messenger.hpp>
+
+namespace rip
+{
+    namespace appendages
+    {
+        DigitalOutput::DigitalOutput(const nlohmann::json& config, const std::map<std::string, int>& command_map, std::shared_ptr<cmdmessenger::Device> device) 
+            : Appendage(config, device)
+            , m_write(createCommand("kWriteDigitalWrite", command_map, cmdmessenger::ArduinoCmdMessenger::makeArgumentString<cmdmessenger::ArduinoCmdMessenger::BooleanType>())
+        {
+        }
+
+        void DigitalOutput::write(bool output)
+        {
+            cmdmessenger::ArduinoCmdMessenger messenger;
+            messenger.send<cmdmessenger::ArduinoCmdMessenger::IntegerType, cmdmessenger::ArduinoCmdMessenger::BooleanType>(m_device, m_write, m_id, output);
+        }
+
+        bool DigitalOutput::stop()
+        {
+
+        }
+
+        bool DigitalOutput::diagnostic()
+        {
+            // todo
+            return true;
+        }
+
+        std::shared_ptr<Appendage> DigitalOutput::create(const nlohmann::json& config, const std::map<std::string, int>& command_map, std::shared_ptr<cmdmessenger::Device> device)
+        {
+            return std::dynamic_pointer_cast<Appendage>(std::shared_ptr<DigitalOutput>(new DigitalOutput(config, command_map, device)));
+        }
+
+    }
+}
