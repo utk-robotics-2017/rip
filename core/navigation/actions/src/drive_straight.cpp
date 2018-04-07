596b6c725190544599bb761c7eceece53ef2c25f
Parker Mitchell <jmitch47@vols.utk.edu>
2018-04-07 17:22:45 -0400

    more bad drive straight

--- core/navigation/actions/src/drive_straight.cpp
+++ core/navigation/actions/src/drive_straight.cpp
@@ -60,18 +60,37 @@ namespace rip
                 r_dynamics.setAcceleration(m_max_accel);
 
                 // just for now...
-                std::vector<units::Distance> dists = m_drivetrain->readEncoders(motors);
-
-                if(    dists[0] >= m_distance || dists[1] >= m_distance
-                    || dists[2] >= m_distance || dists[3] >= m_distance )
+                if(m_use_time)
                 {
-                    m_finished = true;
-                    m_drivetrain->stop();
+                    std::vector<units::Distance> dists = m_drivetrain->readEncoders(motors);
+
+                    if(    dists[0] >= m_distance || dists[1] >= m_distance
+                        || dists[2] >= m_distance || dists[3] >= m_distance )
+                    {
+                        m_finished = true;
+                        m_drivetrain->stop();
+                    }
+                    else
+                    {
+                        m_drivetrain->drive(l_dynamics, r_dynamics);
+                    }
                 }
                 else
                 {
-                    m_drivetrain->drive(l_dynamics, r_dynamics);
+                    m_last_time = std::chrono::system_clock::now();
+                    units::Time diff = std::chrono::duration_cast<std::chrono::milliseconds>(m_last_time - m_start_time).count() * units::ms;
+
+                    if(diff >= m_time)
+                    {
+                        m_finished = true;
+                        m_drivetrain->stop();
+                    }
+                    else
+                    {
+                        m_drivetrain->drive(l_dynamics, r_dynamics);
+                    }
                 }
+
             }
 
             void DriveStraight::setup(nlohmann::json& state)
