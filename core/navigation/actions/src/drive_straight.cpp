a87ffbe3b86a95f3de7881a9a31365e1e4c68504
Parker Mitchell <jmitch47@vols.utk.edu>
2018-04-07 22:26:01 -0400

    Remove trapezoid stuff from Drive Straight until the roboclaw will behave

--- core/navigation/actions/src/drive_straight.cpp
+++ core/navigation/actions/src/drive_straight.cpp
@@ -8,8 +8,11 @@ namespace rip
     {
         namespace actions
         {
-            DriveStraight::DriveStraight(const std::string& name, std::shared_ptr<drivetrains::Drivetrain> drivetrain,
-                 std::shared_ptr<navx::NavX> navx, const units::Distance& distance, const units::Velocity& speed,
+            using drivetrains::Drivetrain;
+			using navx::NavX;
+            
+			DriveStraight::DriveStraight(const std::string& name, std::shared_ptr<Drivetrain> drivetrain,
+                 std::shared_ptr<NavX> navx, const units::Distance& distance, const units::Velocity& speed,
                  double p, double i, double d, units::Acceleration max_accel)
                 : Action(name)
                 , m_use_time(false)
@@ -21,8 +24,8 @@ namespace rip
                 , m_navx(navx)
             {}
 
-            DriveStraight::DriveStraight(const std::string& name, std::shared_ptr<drivetrains::Drivetrain> drivetrain,
-                 std::shared_ptr<navx::NavX> navx, const units::Time& time, const units::Velocity& speed,
+            DriveStraight::DriveStraight(const std::string& name, std::shared_ptr<Drivetrain> drivetrain,
+                 std::shared_ptr<NavX> navx, const units::Time& time, const units::Velocity& speed,
                  units::Acceleration max_accel)
                 : Action(name)
                 , m_use_time(true)
@@ -41,8 +44,6 @@ namespace rip
 
             void DriveStraight::update(nlohmann::json& state)
             {
-                using drivetrains::Drivetrain;
-
                 const std::vector<Drivetrain::Motor> motors = {Drivetrain::Motor::kFrontLeft,
                                                                Drivetrain::Motor::kBackLeft,
                                                                Drivetrain::Motor::kFrontRight,
@@ -64,18 +65,15 @@ namespace rip
                 // just for now...
                 if(!m_use_time)
                 {
-                    l_dynamics.setDeceleration(m_max_accel);
-                    r_dynamics.setDeceleration(m_max_accel);
-
+					// get the encoder values from the Roboclaw & determine the greatest value
                     std::vector<units::Distance> dists = m_drivetrain->readEncoders(motors);
-                    units::Distance threshold = m_distance + m_init_encoder - (1 * units::cm);
-                    units::Distance decel_threshold = (m_distance + m_init_encoder) - (m_speed / m_max_accel) * m_speed / 2;
+					units::Distance max_encoder = *(std::max_element(dists.begin(), dists.end()));
 
-                    l_dynamics.setDistance(m_distance + m_init_encoder - dists[0]);
-                    r_dynamics.setDistance(m_distance + m_init_encoder - dists[2]);
+					// set a threshold for stopping -- this tries to account for the delay in actually stopping
+					// this is quite arbitrary right now but should be kinda close
+                    units::Distance threshold = m_distance + m_init_encoder - (m_speed * (0.06 * units::s));
 
-                    if(    dists[0] >= threshold || dists[1] >= threshold
-                        || dists[2] >= threshold || dists[3] >= threshold )
+                    if(max_encoder >= threshold)
                     {
                         misc::Logger::getInstance()->debug(
                             "Left: {} {} | Right: {} {} | Target: {}",
@@ -85,19 +83,13 @@ namespace rip
 
                         m_finished = true;
                     }
-                    else if(    dists[0] >= decel_threshold || dists[1] >= decel_threshold
-                             || dists[2] >= decel_threshold || dists[3] >= decel_threshold )
-                    {
-                        l_dynamics.setSpeed(0);
-                        r_dynamics.setSpeed(0);
-                    }
                 }
                 else
                 {
                     m_last_time = std::chrono::system_clock::now();
                     units::Time diff = std::chrono::duration_cast<std::chrono::milliseconds>(m_last_time - m_start_time).count() * units::ms;
 
-                    units::Time threshold = m_time - (0.05 * units::s);
+                    units::Time threshold = m_time - (0.06 * units::s);
 
                     if(diff >= threshold)
                     {
@@ -114,9 +106,7 @@ namespace rip
 
             void DriveStraight::setup(nlohmann::json& state)
             {
-                using drivetrains::Drivetrain;
-
-                misc::Logger::getInstance()->debug("Driving Straight");
+				misc::Logger::getInstance()->debug("Driving Straight");
                 m_start_time = std::chrono::system_clock::now();
                 m_finished = false;
 
@@ -125,10 +115,9 @@ namespace rip
                                                                Drivetrain::Motor::kFrontRight,
                                                                Drivetrain::Motor::kBackRight};
 
-               std::vector<units::Distance> dists = m_drivetrain->readEncoders(motors);
+				std::vector<units::Distance> dists = m_drivetrain->readEncoders(motors);
 
-               m_init_encoder = dists[0];
-               for(units::Distance const &dist : dists) if(dist > m_init_encoder) m_init_encoder = dist;
+				m_init_encoder = *(std::max_element(dists.begin(), dists.end()));
             }
 
             void DriveStraight::teardown(nlohmann::json& state)
