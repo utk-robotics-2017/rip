85e598f517e44bfe68c032d9e04e75620ccd75a0
Andrew Messing <amessing@utk.edu>
2018-04-10 09:07:29 -0400

    ir on teensy

---
+++ appendages/include/appendages/ir_2018.hpp
@@ -0,0 +1,63 @@
+#ifndef IR_2018_HPP
+#define IR_2018_HPP
+
+#include <memory>
+#include <map>
+#include <string>
+
+
+#include <cmd_messenger/command.hpp>
+
+#include "appendages/appendage.hpp"
+#include "appendages/appendage_factory.hpp"
+
+#include <json.hpp>
+
+namespace rip
+{
+    namespace appendages
+    {
+        class Ir2018 : public Appendage
+        {
+        public:
+            /**
+             * Reads the ir receiver for the 2018 challenge
+             * @returns Whether A, B, and C are right or left
+             */
+            std::array<bool, 3> read();
+
+            /**
+             * Stop
+             */
+            virtual void stop() override;
+
+            virtual bool diagnostic() override;
+
+        protected:
+            friend class AppendageFactory;
+
+            /**
+             * Function wrapper for the constructor so it can be pointed to
+             *
+             * @param config The config from arduino gen
+             * @param command_map A map of the name of the commands to their enumerations
+             * @param device The connection to the device
+             */
+            static std::shared_ptr<Appendage> create(const nlohmann::json& config, const std::map<std::string, int>& command_map, std::shared_ptr<cmdmessenger::Device> device);
+
+        private:
+            /**
+             * Constructor
+             *
+             * @param config The config from arduino gen
+             * @param command_map A map of the name of the commands to their enumerations
+             * @param device The connection to the device
+             */
+            Ir2018(const nlohmann::json& config, const std::map<std::string, int>& command_map, std::shared_ptr<cmdmessenger::Device> device);
+
+            std::shared_ptr<cmdmessenger::Command> m_read;
+            std::shared_ptr<cmdmessenger::Command> m_read_result;
+        };
+    }
+}
+#endif // IR_2018_HPP
---
+++ appendages/src/ir_2018.cpp
@@ -0,0 +1,48 @@
+#include "appendages/ir_2018.hpp"
+
+#include <utility>
+#include <tuple>
+#include <memory>
+
+#include <cmd_messenger/cmd_messenger.hpp>
+
+namespace rip
+{
+    namespace appendages
+    {
+        Ir2018::Ir2018(const nlohmann::json& config, const std::map<std::string, int>& command_map, std::shared_ptr<cmdmessenger::Device> device)
+            : Appendage(config, device)
+            , m_read(createCommand("kReadIr2018", command_map, cmdmessenger::ArduinoCmdMessenger::makeArgumentString<cmdmessenger::ArduinoCmdMessenger::IntegerType>()))
+            , m_read_result(createCommand("kReadIr2018Result", command_map, cmdmessenger::ArduinoCmdMessenger::makeArgumentString<cmdmessenger::ArduinoCmdMessenger::CharType>()))
+        {
+        }
+
+        std::array<bool, 3> Ir2018::read()
+        {
+            cmdmessenger::ArduinoCmdMessenger messenger;
+            messenger.send<cmdmessenger::ArduinoCmdMessenger::IntegerType>(m_device, m_read, m_id);
+            char data = std::get<0>(messenger.receive<cmdmessenger::ArduinoCmdMessenger::CharType>(m_read_result));
+            std::array<bool, 3> rv;
+            rv[0] = data & 1 << 0;
+            rv[1] = data & 1 << 1;
+            rv[2] = data & 1 << 2;
+            return rv;
+        }
+
+        void Ir2018::stop()
+        {
+
+        }
+
+        bool Ir2018::diagnostic()
+        {
+            // todo
+            return true;
+        }
+
+        std::shared_ptr<Appendage> Ir2018::create(const nlohmann::json& config, const std::map<std::string, int>& command_map, std::shared_ptr<cmdmessenger::Device> device)
+        {
+            return std::dynamic_pointer_cast<Appendage>(std::shared_ptr<DigitalInput>(new Ir2018(config, command_map, device)));
+        }
+    }
+}
