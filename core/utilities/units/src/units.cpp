baf88d00f3cf1708f8172cf08991cd55b9b19265
Ben Klein <robobenklein@gmail.com>
2018-04-13 17:19:56 -0400

    Fixes units::now() for timeoutaction

--- core/utilities/units/src/units.cpp
+++ core/utilities/units/src/units.cpp
@@ -4,6 +4,9 @@ namespace rip
 {
     namespace units
     {
+
+        std::chrono::time_point<std::chrono::system_clock> chrono_time_start = std::chrono::system_clock::now();
+
         double cos(const Angle& lhs)
         {
             return std::cos(lhs.to(rad));
@@ -16,7 +19,9 @@ namespace rip
 
         Time now()
         {
-            return std::chrono::duration_cast< std::chrono::milliseconds >(std::chrono::system_clock::now().time_since_epoch()).count() * ms;
+            // return std::chrono::duration_cast< std::chrono::milliseconds >(std::chrono::system_clock::now().time_since_epoch()).count() * ms;
+            std::chrono::system_clock::duration time_diff = std::chrono::system_clock::now() - chrono_time_start;
+            return std::chrono::duration_cast<std::chrono::milliseconds>(time_diff).count() * ms;
         }
 
     }
